33eaa3742e663efc73d1ce84decc792378460e4b
Grant McDermott <grant.mcdermott@gmail.com>
2023-08-03 20:59:38 +0000

    example script

---
+++ example.R
@@ -0,0 +1,19 @@
+head(mtcars)
+install.packages("data.table")
+install.packages(c("sf", "ggplot2"))
+
+
+library(sf)
+library(ggplot2)
+
+# Use the North Carolina shapefile that comes bundled with sf
+nc_loc = system.file("shape/nc.shp", package="sf")
+## Read the shapefile into R
+nc = st_read(nc_loc, quiet = TRUE)
+nc
+
+ggplot(nc) +
+    geom_sf(aes(fill = AREA), alpha = 0.8, col = "white") +
+    scale_fill_viridis_c(name = "Area") +
+    ggtitle("Counties of North Carolina") +
+    theme_minimal()
